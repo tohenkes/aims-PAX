dtype fix for double precision

src/FHI_AL/procedures/active_learning.py
@@ -34,6 +34,7 @@ from FHI_AL.tools.utilities import (
     get_atomic_energies_from_pt,
     select_best_member,
     atoms_full_copy,
+    dtype_mapping,
     AIMSControlParser,
     ModifyMD
 )
@@ -128,6 +129,7 @@ class PrepareALProcedure:
             self.ensemble = ensemble_from_folder(
                 path_to_models=self.model_dir,
                 device=self.device,
+                dtype=dtype_mapping[self.dtype],
             )
 
             self.training_setups = ensemble_training_setups(
@@ -295,6 +297,7 @@ class PrepareALProcedure:
         self.device = self.mace_settings["MISC"]["device"]
         self.model_dir = self.mace_settings["GENERAL"]["model_dir"]
         self.dtype = self.mace_settings["GENERAL"]["default_dtype"]
+        torch.set_default_dtype(dtype_mapping[self.dtype])
         self.device = self.mace_settings["MISC"]["device"]
         self.atomic_energies_dict = self.mace_settings["ARCHITECTURE"].get("atomic_energies", None)
         self.compute_stress = self.mace_settings.get("compute_stress", False)
@@ -409,6 +412,7 @@ class PrepareALProcedure:
                             path_to_checkpoints=self.checkpoints_dir,
                             z=self.z,
                             seeds_tags_dict=self.seeds_tags_dict,
+                            dtype=self.dtype
                         )
                     else:
                         logging.info("Loading atomic energies from existing ensemble.")
@@ -418,6 +422,7 @@ class PrepareALProcedure:
                         ) = get_atomic_energies_from_ensemble(
                             ensemble=self.ensemble,
                             z=self.z,
+                            dtype=self.dtype
                         )
                     self.update_atomic_energies = True
 
@@ -431,7 +436,8 @@ class PrepareALProcedure:
                         [
                             self.ensemble_atomic_energies_dict[tag][z]
                             for z in self.ensemble_atomic_energies_dict[tag].keys()
-                        ]
+                        ],
+                        dtype=self.dtype
                     ) for tag in self.seeds_tags_dict.keys()}
 
                 logging.info(f'{self.ensemble_atomic_energies_dict[list(self.seeds_tags_dict.keys())[0]]}')
@@ -2038,6 +2044,38 @@ class ALProcedureParallel(ALProcedure):
                 f"Trajectory worker {idx} is waiting for analysis job to finish."
             )
         return None
+
+class ALProcedureGPUParallel(ALProcedureParallel):
+    def __init__(
+        self,
+        mace_settings: dict,
+        al_settings: dict,
+        path_to_control: str = "./control.in",
+        path_to_geometry: str = "./geometry.in",
+    ):
+        super().__init__(
+            mace_settings=mace_settings,
+            al_settings=al_settings,
+            path_to_control=path_to_control,
+            path_to_geometry=path_to_geometry,
+        )
+        # get number of GPUs
+        self.num_gpus = torch.cuda.device_count()
+        self.devices = [torch.device(f"cuda:{i}") for i in range(self.num_gpus)]
+        # create one mpi communcatior per gpu and assign rest to extra communicator
+        self.world_size = WORLD_SIZE
+        self.gpu_world_size = self.num_gpus
+        self.gpu_world_comm = MPI.COMM_WORLD.Split(color=0, key=0)
+        self.extra_comm = MPI.COMM_WORLD.Split(color=1, key=0)
+        self.gpu_ranks = self.gpu_world_comm.Get_size()
+        self.extra_ranks = self.extra_comm.Get_size()
+        self.gpu_rank = self.gpu_world_comm.Get_rank()
+        self.extra_rank = self.extra_comm.Get_rank()
+
+        
+        
+        
+        
         
 
 # TODO: This is not done yet

src/FHI_AL/tools/utilities.py
@@ -549,6 +549,7 @@ def create_mace_dataset(
 def ensemble_from_folder(
     path_to_models: str,
     device: str,
+    dtype,
     compile_mode: str = "default",
     ) -> list:
     """
@@ -571,7 +572,7 @@ def ensemble_from_folder(
     for filename in os.listdir(path_to_models):
         if os.path.isfile(os.path.join(path_to_models, filename)):
             complete_path = os.path.join(path_to_models, filename)
-            model = torch.load(complete_path, map_location=device)
+            model = torch.load(complete_path, map_location=device).to(dtype)
             # One can't train compiled models so not usefull for us.
             #model = torch.compile(
             #        prepare(extract_load)(f=complete_path, map_location=device),
@@ -1304,7 +1305,8 @@ def setup_ensemble_dicts(
 
 def get_atomic_energies_from_ensemble(
     ensemble: dict,
-    z
+    z,
+    dtype: str,
 ):
     """
     Loads the atomic energies from existing ensemble members.
@@ -1312,7 +1314,10 @@ def get_atomic_energies_from_ensemble(
     ensemble_atomic_energies_dict = {}
     ensemble_atomic_energies = {}
     for tag, model in ensemble.items():
-        ensemble_atomic_energies[tag] = np.array(model.atomic_energies_fn.atomic_energies.cpu())
+        ensemble_atomic_energies[tag] = np.array(
+            model.atomic_energies_fn.atomic_energies.cpu(),
+            dtype=dtype_mapping[dtype]
+        )
         ensemble_atomic_energies_dict[tag] = {}
         for i, atomic_energy in enumerate(ensemble_atomic_energies[tag]):
             # TH: i don't know if the atomic energies are really sorted by atomic number
@@ -1325,6 +1330,7 @@ def get_atomic_energies_from_pt(
     path_to_checkpoints: str,
     z,
     seeds_tags_dict: dict,
+    dtype: str,
     convergence: bool = False,
 ):  
     ensemble_atomic_energies_dict = {}
@@ -1337,7 +1343,7 @@ def get_atomic_energies_from_pt(
         
         
         atomic_energies_array = check_pt['model']['atomic_energies_fn.atomic_energies']
-        ensemble_atomic_energies[tag] = np.array(atomic_energies_array.cpu())
+        ensemble_atomic_energies[tag] = np.array(atomic_energies_array.cpu(), dtype=dtype)
         ensemble_atomic_energies_dict[tag] = {}
         for i, atomic_energy in enumerate(ensemble_atomic_energies[tag]):
             # TH: i don't know if the atomic energies are really sorted by atomic number
@@ -1523,7 +1529,8 @@ def update_model_auxiliaries(
         assert z_table is not None
         assert atomic_energies_dict is not None
     
-        energies_train = torch.stack([point.energy.reshape(1)  for point in mace_sets["train"]])
+        energies_train = torch.stack(
+            [point.energy.reshape(1)  for point in mace_sets["train"]])
         zs_train = [
             [
                 z_table.index_to_z(torch.nonzero(one_hot).item()) for one_hot in point.node_attrs