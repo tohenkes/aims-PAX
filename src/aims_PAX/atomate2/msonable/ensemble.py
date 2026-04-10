1aea32882797bb44e8c359be789c72251bd0c084
Andrei Sobolev <sobolev@ms1p.org>
2026-04-10 12:56:57 +0200

    Put more data directly on the ModelTrainer

--- src/aims_PAX/atomate2/msonable/ensemble.py
+++ src/aims_PAX/atomate2/msonable/ensemble.py
@@ -5,15 +5,15 @@ This module contains an MSONable version of the Ensemble model and Ensemble.
 from dataclasses import dataclass
 from typing import Any
 
-from mace.tools import AtomicNumberTable
+from ase import Atoms
 from monty.json import MSONable
 from pymatgen.io.ase import MSONAtoms
 
 from aims_PAX.atomate2.atomic_energies import AtomicEnergies
 from aims_PAX.atomate2.msonable.checkpoint_handler import MSONableCheckpointHandler
 from aims_PAX.atomate2.msonable.ema import MSONableEMA
-from aims_PAX.atomate2.msonable.model import MaceModel
-from aims_PAX.atomate2.msonable.serialization import MSONableModel, wrap
+from aims_PAX.atomate2.msonable.model import MaceModelTrainer, ModelTrainer
+from aims_PAX.atomate2.msonable.serialization import wrap
 from aims_PAX.settings import ModelSettings
 from aims_PAX.tools.model_tools.setup_MACE import setup_mace
 from aims_PAX.tools.model_tools.training_tools import setup_model_training
@@ -22,17 +22,14 @@ from aims_PAX.tools.utilities.data_handling import KeySpecification
 
 @dataclass
 class Ensemble(MSONable):
-    models: dict[str, MSONableModel]
+    models: dict[str, ModelTrainer]
     training_setups: dict[str, dict[str, Any]]
-    ase_sets: dict[str, dict[str, list[Any]]]
-    model_sets: dict[str, dict[str, Any]]
-    key_specification: KeySpecification
 
     @classmethod
     def from_scratch(cls,
                      tags: list[str],
                      model_settings: ModelSettings,
-                     atomic_energies: AtomicEnergies,
+                     atomic_energies: dict[str, AtomicEnergies],
                      model_inputs: dict[str, Any],
                      key_specification: KeySpecification) -> "Ensemble":
         """
@@ -51,23 +48,29 @@ class Ensemble(MSONable):
         # can we put calculate model inputs here?
         ensemble = {}
         training_setups = {}
-        ase_sets = {}
-        model_sets = {}
 
         model_choice = model_settings.GENERAL.model_choice.lower()
         for tag in tags:
             if model_choice == "mace":
-                model = setup_mace(model_settings, model_inputs["z_table"], atomic_energies.get(tag))
-                ensemble[tag] = MaceModel.from_parent(
+                model = setup_mace(
+                    model_settings,
+                    model_inputs["z_table"],
+                    atomic_energies[tag].as_dict()
+                )
+                ensemble[tag] = MaceModelTrainer.from_model(
                     model,
                     settings=model_settings,
+                    tag=tag,
+                    head="Default",
                     zs=model_inputs["z_table"].zs,
-                    atomic_energies=atomic_energies.get(tag)
+                    atomic_energies=atomic_energies[tag],
+                    key_specification=key_specification
                 )
             else:
                 raise NotImplementedError(f"{model_choice} is not supported yet. "
                                           f"Supported models are: ['mace']")
-            # TODO: checkpoints_dir is unneeded as model settings is already there
+            # leave training setups separate for now
+            # should it live on a Model?
             training_setups[tag] = setup_model_training(
                 settings=model_settings,
                 model=model,
@@ -87,30 +90,42 @@ class Ensemble(MSONable):
                 if key in training_setups[tag]:
                     training_setups[tag][key] = wrap(training_setups[tag][key])
 
-            ase_sets[tag] = {"train": [], "valid": []}
-            # TODO: add multihead model support
-            model_sets[tag] = {"train": [], "valid": {"Default": []}}
         return cls(models=ensemble,
-                   training_setups=training_setups,
-                   ase_sets=ase_sets,
-                   model_sets=model_sets,
-                   key_specification=key_specification,
-                   zs=model_inputs["z_table"].zs)
+                   training_setups=training_setups
+                  )
 
     @classmethod
     def from_restart_data(cls):
         raise NotImplementedError
 
-    def get_model(self, tag: str) -> MSONableModel:
+    def get_model(self, tag: str) -> ModelTrainer:
         return self.models[tag]
 
     def get_training_setup(self, tag: str) -> dict[str, Any]:
         return self.training_setups[tag]
 
-    @property
-    def z_table(self):
-        return AtomicNumberTable(self.zs)
-
-    def update_datasets(self, tag, train_set: list[MSONAtoms], test_set: list[MSONAtoms]):
-        """Updates datasets for the training and testing of the model.
+    def update_datasets(self,
+                        training_sets: dict[str, list[MSONAtoms | Atoms]]  = None,
+                        valid_sets: dict[str, list[MSONAtoms | Atoms]] = None
+                        ):
         """
+        Updates the datasets of the models in the ensemble.
+        Args:
+            training_sets: a dict of training sets, keyed by tag.
+            valid_sets: a dict of validation sets, keyed by tag.
+
+        """
+        training_sets = training_sets or {}
+        valid_sets = valid_sets or {}
+        # update models' datasets
+        for tag in self.models:
+            if tag in training_sets:
+                self.models[tag].update_train_set(training_sets[tag])
+            if tag in valid_sets:
+                self.models[tag].update_valid_set(valid_sets[tag])
+
+    def train(self):
+        """Train all models in the ensemble."""
+
+        for tag in self.models:
+            self.models[tag].train(self.training_setups[tag])
--- src/aims_PAX/atomate2/msonable/model.py
+++ src/aims_PAX/atomate2/msonable/model.py
@@ -1,49 +1,153 @@
 """
 This is a module with an MSONable version of the Model classes.
 """
-from dataclasses import dataclass, Field
+import logging
+from dataclasses import dataclass
+from typing import Any
 
+import numpy as np
+from ase import Atoms
+from mace.data import KeySpecification
 from mace.tools import AtomicNumberTable
 from monty.json import MSONable
+from pymatgen.io.ase import MSONAtoms
 
-from .serialization import MSONableMACE
+from .serialization import MSONableMACE, MSONableModel
+from ..atomic_energies import AtomicEnergies
 from ...settings import ModelSettings
+from ...tools.utilities.data_handling import create_model_dataset, create_dataloader
+from ...tools.utilities.utilities import update_model_auxiliaries
 
 
 @dataclass
-class MaceModel(MSONable):
-    model: MSONableMACE
+class ModelTrainer(MSONable):
+    model: MSONableModel
     settings: ModelSettings
+    tag: str
     zs: list[int]
-    atomic_energies: dict[int, float]
-    train_set_ase: list = None
-    valid_set_ase: list = None
+    atomic_energies: AtomicEnergies
+    key_specification: KeySpecification
+    train_set: list = None
+    valid_set: list = None
+    valid_losses: float = np.inf
 
     def __post_init__(self):
-        self.train_set_ase = []
-        self.valid_set_ase = []
-
-    def __getattr__(self, name):
-        # avoid infinite recursion if 'model' itself isn't set yet
-        if name == "model":
-            raise AttributeError(name)
-        return getattr(self.model, name)
-
-    @classmethod
-    def from_parent(cls, instance, **kwargs):
-        return cls(model=MSONableMACE.from_parent(instance), **kwargs)
+        """Sets the train and valid sets to empty lists."""
+        self.train_set = []
+        self.valid_set = []
 
     @property
     def z_table(self):
         return AtomicNumberTable(self.zs)
 
-    @property
-    def train_set(self):
-        return self.train_set_ase
+    def update_train_set(self, train_set: list[MSONAtoms | Atoms]):
+        train_set = [a if isinstance(a, MSONAtoms) else MSONAtoms(a) for a in train_set]
+        self.train_set += train_set
+
+    def update_valid_set(self, valid_set: list[MSONAtoms | Atoms]):
+        valid_set = [a if isinstance(a, MSONAtoms) else MSONAtoms(a) for a in valid_set]
+        self.valid_set += valid_set
+
+    def train(self, training_setup: dict[str, Any]):
+        """Train a model on given model sets."""
+        raise NotImplementedError
+
+
+@dataclass
+class MaceModelTrainer(ModelTrainer):
+    model: MSONableMACE
+    head: str = "Default"
+
+    @classmethod
+    def from_model(cls, instance, **kwargs):
+        return cls(model=MSONableMACE.from_parent(instance), **kwargs)
 
     @property
-    def valid_set(self):
-        return self.valid_set_ase
+    def train_model_set(self):
+        return self._create_model_set(self.train_set)
 
-    def update_datasets(self, train_set: list, valid_set: list):
-        pass
+    @property
+    def valid_model_set(self):
+        return {self.head: self._create_model_set(self.valid_set)}
+
+    @property
+    def batch_size(self):
+        """Returns the batch size for training."""
+        return (
+            1
+            if len(self.train_set) < self.settings.TRAINING.batch_size
+            else self.settings.TRAINING.batch_size
+            )
+
+    @property
+    def valid_batch_size(self):
+        """Returns the batch size for validation."""
+        smallest_valid_set = 0
+        # This should be subclassed for a multihead model
+        # MACE models are always single head
+        for valid_set in self.valid_model_set.values():
+            if (
+                    smallest_valid_set == 0
+                    or len(valid_set) < smallest_valid_set
+            ):
+                smallest_valid_set = len(valid_set)
+
+        return (
+            1
+            if smallest_valid_set
+               < self.settings.TRAINING.valid_batch_size
+            else self.settings.TRAINING.valid_batch_size
+            )
+
+    def train(self, training_setup: dict[str, Any]):
+        """Trains a model on given model sets"""
+        train_model_set = self.train_model_set
+        valid_model_set = self.valid_model_set
+        # create dataloaders
+        train_loader, valid_loader = create_dataloader(
+            train_model_set,
+            valid_model_set,
+            self.batch_size,
+            self.valid_batch_size,
+        )
+        model_sets = dict(
+            train=train_model_set,
+            valid=valid_model_set,
+            train_loader=train_loader,
+            valid_loader=valid_loader
+        )
+        logging.info(
+            f"Training set size for '{self.tag}': {len(train_model_set)}; "
+            f"Validation set size: "
+        )
+        for head, valid_set in valid_model_set.items():
+            logging.info(f" - Head '{head}': {len(valid_set)}")
+
+    def _update_auxiliaries(self, model_sets: dict[str, Any]):
+        update_model_auxiliaries(
+            model=self.model,
+            model_choice="mace",
+            model_sets=model_sets,
+            # scaling is set for MACE models only
+            scaling=self.settings.ARCHITECTURE.scaling,
+            z_table=self.z_table,
+            atomic_energies_list=self.ensemble_atomic_energies[self.tag],
+            atomic_energies_dict=self.ensemble_atomic_energies_dict[self.tag],
+            update_atomic_energies=self.update_atomic_energies,
+            update_avg_num_neighbors=self.settings.TRAINING.update_avg_num_neighbors,
+            dtype=self.settings.GENERAL.default_dtype,
+            device=self.settings.MISC.device,
+        )
+
+
+    def _create_model_set(self, data: list[MSONAtoms]):
+        return create_model_dataset(
+            data=data,
+            z_table=self.z_table,
+            seed=self.settings.GENERAL.seed,
+            r_max=self.settings.ARCHITECTURE.r_max,
+            # r_max_lr is used only in SO3LR / So3krates
+            r_max_lr=None,
+            key_specification=self.key_specification
+        )
+
